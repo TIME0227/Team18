c48cc55cbc3d69c99494189f9fb931c47b446d7c
TIME Desktop <newspaper0227@gmail.com>
2024-09-22 12:51:21 +0900

    구매 지원받은 에셋 추가

--- UZU_Tiger_Test/Assets/Scripts/Main/MainController.cs
+++ UZU_Tiger_Test/Assets/Scripts/Main/MainController.cs
@@ -58,7 +58,7 @@ public class MainController : MonoBehaviour
                         // MainCamera pos & rot 조정
                         mainCamera.transform.position = new Vector3(TestNPC2.transform.position.x - 0.1f, TestNPC2.transform.position.y - 0.27f, TestNPC2.transform.position.z + 7.16f);
                         mainCamera.transform.rotation = Quaternion.Euler(8.334f, 180, 0);
-                        // OpenAIController의 systeMessage 변경
+                        // OpenAIController의 systmMessage 변경
                         openAIController.systemMessage = "너는 사용자의 친한 친구야. 너는 아주 착하고 밝고 순수하고 친절하고 친구의 얘기를 잘 들어줘. 친구를 비판하지 않고 수용하고 존중하며, 친구의 일에 관심이 많고 같이 하고 싶은 것도 많아. 즐거운 일엔 같이 웃고 슬픈 일은 같이 슬퍼하는 등 감정을 함께 공유할 수 있는 친구야.  친근한 구어체를 사용하고(반말), 150토큰 이내로 끝내줘. 모든 답변의 끝에는 그 답변에 어울리는 제스처를 덧붙여. 제스처에는 \\\"Attack, Bounce, Clicked, Death, Eat, Fear, Fly, Hit, Idle_A, Idle_B, Idle_C, Jump, Roll, Run, Sit, Spin, Swim, Walk\\\" 18가지가 있어. \\\"#\\\"를 단어 앞에 붙여서 구분할 수 있도록 답변 끝에 덧붙여. 예시로 \\\"#Attack\\\"과 같이 덧붙이면 돼. 제스처가 없으면 오류가 나기 때문에 답변마다 반드시 18개 중 하나를 붙여야 해. 답변할 때 제스처를 표시할 때 외에는 \\”#\\”을 사용해선 안 돼.";
                         Debug.Log(hit.transform.name);
                         break;
